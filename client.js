251a4fa8efbce74d7993c93f74744f1d764bd98d
Dan Habot <dan.habot@gmail.com>
2020-01-08 23:01:40 -0500

    Created client

---
+++ client.js
@@ -0,0 +1,44 @@
+const ws = require('ws')
+const classes = require('multiple-extend');
+const EventEmitter = require('events')
+const { Writable } = require('stream')
+
+module.exports = class Client extends classes(EventEmitter, Writable) {
+  constructor(address, opts) {
+    super()
+
+    this.ws = new ws(address, 'webcast')
+    this.ws.once('open', () => {
+      this.ws.send(JSON.stringify({
+        type: "hello",
+        data: opts
+      }))
+    })
+    
+  }
+
+  _send(d) {
+    if (this.ws.readyState == ws.OPEN) {
+      this.ws.send(d)
+    }
+  }
+
+  _sendText(type, data) {
+    this._send(JSON.stringify({
+      type, data
+    }))
+  }
+
+  _sendBinary(data) {
+    this._send(data)
+  }
+
+  _write(chunk, encoding, done) {
+    this._sendBinary(chunk)
+    done()
+  }
+
+  sendMeta(data) {
+    this._sendText('metadata', JSON.stringify(data))
+  }
+}
